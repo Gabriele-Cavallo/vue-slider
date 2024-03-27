9b0b19234ae6170950e482fb9549a0469a4c5146
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-27 15:25:18 +0100

    feat: add nextImage function

--- js/script.js
+++ js/script.js
@@ -28,6 +28,14 @@ console.log(slides);
 
 // Partendo dal markup fornito nello starter pack, rifare lo slider ma questa volta usando Vue.
 
+// Bonus:
+
+// 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+
+// 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+
+// 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
+
 const { createApp } = Vue;
 
 createApp({
@@ -60,6 +68,12 @@ createApp({
         };
     },
     methods: {
-    
+        nextImage: function(){
+            if (this.activeImage < this.slides.length -1){
+                this.activeImage++;
+            }else {
+                this.activeImage = 0;
+            }
+        },
     }
 }).mount('#app');
